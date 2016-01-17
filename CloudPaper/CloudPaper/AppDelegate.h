5480f9528a26eade01f892d40c5661c8b0395061
Phoenix <x.rhythm@qq.com>
2016-01-17 16:47:33 +0800

    初始化

---
+++ CloudPaper/CloudPaper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CloudPaper
+//
+//  Created by Phoenix on 16/1/17.
+//  Copyright © 2016年 Phoenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CloudPaper/CloudPaper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CloudPaper
+//
+//  Created by Phoenix on 16/1/17.
+//  Copyright © 2016年 Phoenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
